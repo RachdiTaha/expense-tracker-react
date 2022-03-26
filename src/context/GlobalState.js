a8fd484ee5b0b328c2effc414d18a90d7d06575a
RachdiTaha <rachditaha@gmail.com>
2022-03-26 13:39:11 +0100

    other commit

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -3,7 +3,7 @@ import AppReducer from "./AppReducer";
 
 // Initial state
 const initialState = {
-  transaction: [
+  transactions: [
     { id: 1, text: "Flower", amount: -20 },
     { id: 2, text: "Salary", amount: 300 },
     { id: 3, text: "Book", amount: -10 },
